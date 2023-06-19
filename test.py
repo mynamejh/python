39ac973a295e4a94f631eb6cdebb0ef269cdf1b1
jihyunHwang <wlgus1598@naver.com>
2023-06-20 00:25:52 +0900

    0620_1

--- test.py
+++ test.py
@@ -1,16 +1,11 @@
 import csv
-f = open('../Seoul.csv','r',encoding='cp949')
-data = csv.reader(f,delimiter=',')
+f = open('../Seoul.csv')
+data = csv.reader(f)
 header = next(data)
-
-max_temp=-999
-max_date= ''
-for row in data:
-    if row[-1]=='':
-        row[-1]==-999
-        row[-1]=float(row[-1])
-
-        if max_temp<row[-1]:
-            max_date = row[0]
-            max_temp = row[-1]
+for row in data :
+    # row[-1] 은 리스트 마지막 요소를 선택하는 indexing 방법
+    # row[-1] : 각 행의 마지막 열 의미
+    # 이때, csv 파일에서 읽어온 값들은 문자열 형태로 저장되기 때문에 형 변환 = float() 필요
+    row[-1] = float(row[-1])
+    print(row)
 f.close()
